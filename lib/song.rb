4231dd6ed26829068c0bab902ab210bbfa0c5b20
Annie Truex <artruex@gmail.com>
2019-08-13 18:00:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,6 +32,8 @@ class Song
     self.all.detect{|s| s.name == song_name}
   end
   
+  def self.find_or_create_by_name(song_name)
+  
  # def self.new_by_name(song_name)
   #  song.name = song_name
 
