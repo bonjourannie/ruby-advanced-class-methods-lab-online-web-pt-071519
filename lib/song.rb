37d479113275f76f50fa55d7c759dc7ca604558c
Annie Truex <artruex@gmail.com>
2019-08-13 17:52:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,7 +28,11 @@ class Song
     song
   end
   
-  def self.new_by_name(song_name)
-    song.name = song_name
+  def self.find_by_name(song_name)
+    
+  end
+  
+ # def self.new_by_name(song_name)
+  #  song.name = song_name
 
 end
